19bcae75b3f9f405d815c45a909fd3a04e4fbc2f
popi popi po po pi po <diana.bakieva69@gmail.com>
2024-04-24 14:05:34 +0500

    Update lab41_sqlite.h

--- lab4/lab41_sqlite.h
+++ lab4/lab41_sqlite.h
@@ -1,1 +1,10 @@
+#ifndef LAB4_SQLITE_H
+#define LAB4_SQLITE_H
+
+#define DB_FILE "mydb.db"
+
+void sqlite_get_data();
+void sqlite_update(int book_id, const char *book_title, const char *book_description, int book_pages);
+
+#endif
 
