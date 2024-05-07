bdd5da8dd0b6ed179b139f3dbf233f6a0438b568
popi popi po po pi po <diana.bakieva69@gmail.com>
2024-05-08 00:49:26 +0500

    Update lab41_sqlite.c

--- lab4/lab41_sqlite.c
+++ lab4/lab41_sqlite.c
@@ -2,31 +2,47 @@
 #include <stdio.h>
 #include "lab4_sqlite.h"
 
+static sqlite3 *db = NULL;
+
 int callback(void *, int, char **, char **);
 
-void sqlite_get_data()
-{
-    sqlite3 *db;
-    char *err_msg = 0;
+void sqlite_open_db() {
     int rc;
-    if (SQLITE_OK != (rc = sqlite3_open(DB_FILE, &db)))
-    {
+    rc = sqlite3_open(DB_FILE, &db);
+    if (rc != SQLITE_OK) {
         fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
         sqlite3_close(db);
     }
+}
 
+void sqlite_close_db() {
+    if (db != NULL) {
+        sqlite3_close(db);
+        db = NULL;
+    }
+}
+
+void sqlite_get_data() {
+    if (db == NULL) {
+        fprintf(stderr, "Database is not open.\n");
+        return;
+    }
+    
     char *sql = "SELECT \
                   id, \
                   name \
               FROM \
                   your_table;";
-    rc = sqlite3_exec(db, sql, callback, NULL, &err_msg);
-    if (rc != SQLITE_OK)
-    {
+    char *err_msg = 0;
+    int rc = sqlite3_exec(db, sql, callback, NULL, &err_msg);
+    if (rc != SQLITE_OK) {
         fprintf(stderr, "Failed to select data\n");
         fprintf(stderr, "SQLite error: %s\n", err_msg);
         sqlite3_free(err_msg);
-        sqlite3_close(db);
     }
-    sqlite3_close(db);
+}
+
+void sqlite_exit() {
+    sqlite_close_db();
+    printf("Exiting...\n");
 }
