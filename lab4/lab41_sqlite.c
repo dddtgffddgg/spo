eb75afed4732a935b7a905c5c39f92846c5eab79
popi popi po po pi po <diana.bakieva69@gmail.com>
2024-05-08 13:42:54 +0500

    Update lab41_sqlite.c

--- lab4/lab41_sqlite.c
+++ lab4/lab41_sqlite.c
@@ -4,9 +4,11 @@
 
 static sqlite3 *db = NULL;
 
-int callback(void *, int, char **, char **);
-
 void sqlite_open_db() {
+    if (db != NULL) {
+        fprintf(stderr, "Database is already open.\n");
+        return;
+    }
     int rc;
     rc = sqlite3_open(DB_FILE, &db);
     if (rc != SQLITE_OK) {
@@ -16,60 +18,12 @@ void sqlite_open_db() {
 }
 
 void sqlite_close_db() {
-    if (db != NULL) {
-        sqlite3_close(db);
-        db = NULL;
-    }
-}
-
-void sqlite_get_data() {
     if (db == NULL) {
         fprintf(stderr, "Database is not open.\n");
         return;
     }
-    
-    char *sql = "SELECT \
-                  id, \
-                  name \
-              FROM \
-                  your_table;";
-    char *err_msg = 0;
-    int rc = sqlite3_exec(db, sql, callback, NULL, &err_msg);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Failed to select data\n");
-        fprintf(stderr, "SQLite error: %s\n", err_msg);
-        sqlite3_free(err_msg);
-    }
-}
-
-void sqlite_exit() {
-    sqlite_close_db();
-    printf("Exiting...\n");
-}
-
-
-
-
-#include <stdio.h>
-#include <sqlite3.h>
-#include "lab4_sqlite.h"
-
-static sqlite3 *db = NULL;
-
-void sqlite_open_db() {
-    int rc;
-    rc = sqlite3_open(DB_FILE, &db);
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
-        sqlite3_close(db);
-    }
-}
-
-void sqlite_close_db() {
-    if (db != NULL) {
-        sqlite3_close(db);
-        db = NULL;
-    }
+    sqlite3_close(db);
+    db = NULL;
 }
 
 void sqlite_get_data() {
@@ -92,4 +46,3 @@ void sqlite_close_connection() {
     sqlite_close_db();
     printf("Connection closed.\n");
 }
-
