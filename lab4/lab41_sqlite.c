7a7e2d7678a2f287d86667b4e3cfcaf282dbd812
popi popi po po pi po <diana.bakieva69@gmail.com>
2024-05-08 13:32:27 +0500

    Update lab41_sqlite.c

--- lab4/lab41_sqlite.c
+++ lab4/lab41_sqlite.c
@@ -46,3 +46,50 @@ void sqlite_exit() {
     sqlite_close_db();
     printf("Exiting...\n");
 }
+
+
+
+
+#include <stdio.h>
+#include <sqlite3.h>
+#include "lab4_sqlite.h"
+
+static sqlite3 *db = NULL;
+
+void sqlite_open_db() {
+    int rc;
+    rc = sqlite3_open(DB_FILE, &db);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+    }
+}
+
+void sqlite_close_db() {
+    if (db != NULL) {
+        sqlite3_close(db);
+        db = NULL;
+    }
+}
+
+void sqlite_get_data() {
+    if (db == NULL) {
+        fprintf(stderr, "Database is not open.\n");
+        return;
+    }
+    // Ваш код для запроса данных из базы данных
+}
+
+void sqlite_update(int compid, const char *compname, float price) {
+    if (db == NULL) {
+        fprintf(stderr, "Database is not open.\n");
+        return;
+    }
+    // Ваш код для обновления данных в базе данных
+}
+
+void sqlite_close_connection() {
+    sqlite_close_db();
+    printf("Connection closed.\n");
+}
+
