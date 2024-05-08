03bff2803c32710e0a47dd52593046c60b7dbe83
popi popi po po pi po <diana.bakieva69@gmail.com>
2024-05-08 14:22:27 +0500

    Update lab41.c

--- lab4/lab41.c
+++ lab4/lab41.c
@@ -1,10 +1,13 @@
 #include <sqlite3.h>
 #include <stdio.h>
-#include "lab4_sqlite.h"
+#include "lab41_sqlite.h"
+
+#define DB_FILE "mydb.db"
 
 int main() {
     sqlite_open_db();
     sqlite_get_data();
-    sqlite_close_connection();
+    sqlite_close_db();
+    sqlite_exit();
     return 0;
 }
