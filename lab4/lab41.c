aadf5573ccd9897b9dd494b5cde93b16bea35691
popi popi po po pi po <diana.bakieva69@gmail.com>
2024-05-15 11:31:18 +0500

    Update lab41.c

--- lab4/lab41.c
+++ lab4/lab41.c
@@ -6,7 +6,15 @@
 
 int main() {
     sqlite_open_db();
+    
+    // Вставка данных
+    sqlite_insert_author("New Author");
+    sqlite_insert_publisher("New Publisher", "New City");
+    sqlite_insert_book("New Book", "New Annotation", 123, "ISBN123", 1, 1);
+    
+    // Получение данных
     sqlite_get_data();
+    
     sqlite_close_db();
     sqlite_exit();
     return 0;
