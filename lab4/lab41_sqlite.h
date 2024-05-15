74963a8ba614d3d5ba5c08b9aacc6b3c747a80ff
popi popi po po pi po <diana.bakieva69@gmail.com>
2024-05-15 11:30:47 +0500

    Update lab41_sqlite.h

--- lab4/lab41_sqlite.h
+++ lab4/lab41_sqlite.h
@@ -8,6 +8,9 @@
 void sqlite_open_db();
 void sqlite_close_db();
 void sqlite_get_data();
+void sqlite_insert_author(const char *name);
+void sqlite_insert_publisher(const char *name, const char *city);
+void sqlite_insert_book(const char *title, const char *annotation, int pages, const char *isbn, int author_id, int publisher_id);
 void sqlite_exit();
 
 #endif
