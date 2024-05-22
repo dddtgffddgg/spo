0f543828ac863205684e829a1cf2e4decdc9fc30
popi popi po po pi po <diana.bakieva69@gmail.com>
2024-05-22 13:02:35 +0500

    Update lab63.c

--- lab63.c
+++ lab63.c
@@ -1,28 +1,46 @@
 #include <stdio.h>
-#include <omp.h>
 #include <math.h>
-
-#define NUM_THREADS 4
+#include <omp.h>
 
 long double f(long double x) {
-    return sin(x); // Замените это на вашу функцию
+    if (x >= 0 && x <= 1) {
+        return cosl(x + x * x * x);
+    } else if (x > 1 && x <= 2) {
+        return expl(-x * x) - x * x + 2 * x;
+    }
+    return 0; // For safety, though it's outside the given range.
+}
+
+long double integrate(long double (*func)(long double), long double a, long double b, int n) {
+    long double h = (b - a) / n;
+    long double sum = 0.0;
+
+    #pragma omp parallel for reduction(+:sum)
+    for (int i = 0; i < n; ++i) {
+        long double x = a + (i + 0.5) * h;
+        sum += func(x);
+    }
+
+    return sum * h;
 }
 
 int main() {
-    long double a = 0.0; // Начальная граница интегрирования
-    long double b = 1.0; // Конечная граница интегрирования
-    long double dx = 1e-7; // Шаг интегрирования
+    long double a = 0.0, b = 2.0;
+    long double epsilon = 1e-15;
+    int n = 1000; // Initial number of steps
+    long double I_n, I_2n, delta;
 
-    long double integral = 0.0;
-    long double x;
+    I_n = integrate(f, a, b, n);
+    I_2n = integrate(f, a, b, 2 * n);
+    delta = fabsl(I_2n - I_n) / 3.0;
 
-    #pragma omp parallel for num_threads(NUM_THREADS) private(x) reduction(+:integral)
-    for (int i = 0; i < (int)((b - a) / dx); i++) {
-        x = a + i * dx;
-        integral += f(x) * dx;
+    while (delta >= epsilon) {
+        n *= 2;
+        I_n = I_2n;
+        I_2n = integrate(f, a, b, 2 * n);
+        delta = fabsl(I_2n - I_n) / 3.0;
     }
 
-    printf("Integral value: %.20Lf\n", integral);
-
+    printf("Approximated integral value: %.20Lf\n", I_2n);
     return 0;
 }
