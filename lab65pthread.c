8f32f280b5c5f44d573ca8f0c3a84918a55174c5
popi popi po po pi po <diana.bakieva69@gmail.com>
2024-05-22 14:18:56 +0500

    Update lab65pthread.c

--- lab65pthread.c
+++ lab65pthread.c
@@ -2,65 +2,65 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define N 10
-#define NUM_THREADS 4
+#define N 3
+#define NUM_THREADS 3
 
 int matrix[N][N];
 int det = 0;
 pthread_mutex_t mutex;
 
 typedef struct {
-    int row;
+    int col;
     int n;
 } ThreadData;
-
-void *calculate_determinant(void *args) {
-    ThreadData *data = (ThreadData *)args;
-    int sub_det = 0;
-    int temp[N][N];
-    int sign = (data->row % 2 == 0) ? 1 : -1;
-    
-    for (int i = 0; i < data->n; i++) {
-        int subi = 0;
-        for (int j = 1; j < data->n; j++) {
-            int subj = 0;
-            for (int k = 0; k < data->n; k++) {
-                if (k == i) continue;
-                temp[subi][subj] = matrix[data->row * data->n + j][k];
-                subj++;
-            }
-            subi++;
-        }
-        sub_det = sub_det + sign * matrix[data->row * data->n][i] * determinant(temp, data->n - 1);
-        sign = -sign;
-    }
-    
-    pthread_mutex_lock(&mutex);
-    det += sub_det;
-    pthread_mutex_unlock(&mutex);
-    pthread_exit(NULL);
-}
 
 int determinant(int m[N][N], int n) {
     if (n == 1) return m[0][0];
     if (n == 2) return m[0][0] * m[1][1] - m[0][1] * m[1][0];
     
-    pthread_t threads[NUM_THREADS];
-    ThreadData thread_data[NUM_THREADS];
-    int row = 0;
+    int temp[N][N];
+    int sign = 1;
+    int det = 0;
     
-    for (int i = 0; i < NUM_THREADS; i++) {
-        thread_data[i].row = row;
-        thread_data[i].n = n;
-        pthread_create(&threads[i], NULL, calculate_determinant, (void *)&thread_data[i]);
-        row++;
+    for (int i = 0; i < n; i++) {
+        int subi = 0;
+        for (int j = 1; j < n; j++) {
+            int subj = 0;
+            for (int k = 0; k < n; k++) {
+                if (k == i) continue;
+                temp[subi][subj] = m[j][k];
+                subj++;
+            }
+            subi++;
+        }
+        det += sign * m[0][i] * determinant(temp, n - 1);
+        sign = -sign;
     }
-    
-    for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
-    }
-    
     return det;
+}
+
+void *calculate_determinant(void *args) {
+    ThreadData *data = (ThreadData *)args;
+    int temp[N][N];
+    int sign = (data->col % 2 == 0) ? 1 : -1;
+    int sub_det = 0;
+
+    int subi = 0;
+    for (int j = 1; j < data->n; j++) {
+        int subj = 0;
+        for (int k = 0; k < data->n; k++) {
+            if (k == data->col) continue;
+            temp[subi][subj] = matrix[j][k];
+            subj++;
+        }
+        subi++;
+    }
+    sub_det = sign * matrix[0][data->col] * determinant(temp, data->n - 1);
+    
+    pthread_mutex_lock(&mutex);
+    det += sub_det;
+    pthread_mutex_unlock(&mutex);
+    pthread_exit(NULL);
 }
 
 int main() {
@@ -72,7 +72,19 @@ int main() {
         }
     }
     
-    int det = determinant(matrix, N);
+    pthread_t threads[NUM_THREADS];
+    ThreadData thread_data[NUM_THREADS];
+    
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_data[i].col = i;
+        thread_data[i].n = N;
+        pthread_create(&threads[i], NULL, calculate_determinant, (void *)&thread_data[i]);
+    }
+    
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    
     printf("Determinant of the matrix: %d\n", det);
     
     pthread_mutex_destroy(&mutex);
